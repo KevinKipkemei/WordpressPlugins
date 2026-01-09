e6fab7667b43b3b83005a86b58150128d3818ee8
Tester <test@example.com>
2026-01-09 12:25:33 +0300

    Simple Copyright Plugin (Appending)

---
+++ copright-note/copyright-note.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Plugin Name: Copyright Note
+ * Description: A simple plugin that appends a copyright note on a post
+ * Version : 1.0.0
+ * Author : Kevin Kipkemei
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+;
+
+/**
+ * @param string $content
+ * @return string
+ */
+
+function cn_append($content)
+{
+
+    if (!is_single() || !is_main_query()) {
+        return $content;
+    }
+
+    $name = 'Kevin Kipkemei';
+    $year = date('Y');
+    $copyright_message_HTML = sprintf('<div style="margin-top: 30px; padding-top: 10px; color: #666; font-size: 12px">
+    <p> %s %s. All rights reserved.</p>
+    </div>', $name, $year);
+
+    return $content . $copyright_message_HTML;
+}
+
+add_filter('the_content', 'cn_append');
